abaca37349f0a17a426ad436774727b2041eb54b
maca <poissontoxique@gmail.com>
2024-07-04 19:19:51 -0300

    Fix Cartas + Fix botones Comida (Tienda) + cinemáticas C06 y GE

--- PROGRAMACION/Unity2021/Zoo-no-logico/Assets/Scripts/MonedasTotales.cs
+++ PROGRAMACION/Unity2021/Zoo-no-logico/Assets/Scripts/MonedasTotales.cs
@@ -15,6 +15,8 @@ public class MonedasTotales : MonoBehaviour {
 
     public GameObject Cosoprueba;
 
+    [SerializeField] private Comida comidaScript;
+
     [SerializeField] private GameObject ANALYTICS;
 
     // Start is called before the first frame update
